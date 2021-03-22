52f257a02d7be46675e99012df97aaf0d7a5fd1e
Your Name <you@example.com>
2021-03-22 12:47:28 +0900

    3. 인스타 클론코딩 - User Entity 생성 완료

---
+++ src/main/java/com/cos/costargram/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.cos.costargram.domain.user;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(length = 30, unique = true)
+	private String username;
+	
+	@JsonIgnore
+	private String password;
+	
+	private String name; // 이름
+	private String website; // 웹사이트
+	private String bio; // 자기소개
+	private String email; // 이메일
+	private String phone; // 휴대전화
+	private String gender; // 성별
+	
+	private String profileImage; // 프로필 이미지
+	private String provider; // 제공자 Google, FaceBook, Naver..
+	
+	private String role; // USER, ADMIN
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
