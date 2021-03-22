31e1f07381e2ad14c9dc678455f1052e2bb83ebb
Your Name <you@example.com>
2021-03-22 12:20:15 +0900

    1. 인스타 클론 코딩- UI 완료

---
+++ src/main/java/com/cos/costargram/CostagramApplication.java
@@ -0,0 +1,13 @@
+package com.cos.costargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostagramApplication.class, args);
+	}
+
+}
