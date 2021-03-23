a1f65bb9ba44bf383140c7ebbe5995c54dd62ac1
김상길 <soppwy3@gmail.com>
2021-03-23 10:32:52 +0900

    5. 인스타그램 글론코딩 - 회원가입 완료

---
+++ src/main/java/com/cos/costargram/service/AuthService.java
@@ -0,0 +1,28 @@
+package com.cos.costargram.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.costargram.domain.user.User;
+import com.cos.costargram.domain.user.UserRepository;
+import com.cos.costargram.web.auth.dto.UserJoinReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+	
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Transactional
+	public void 회원가입(UserJoinReqDto userJoinReqDto) {
+		String rawPassword = userJoinReqDto.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		userJoinReqDto.setPassword(encPassword);
+		
+		userRepository.save(userJoinReqDto.toEntity());
+	}
+}
--- src/main/java/com/cos/costargram/web/AuthController.java
+++ src/main/java/com/cos/costargram/web/AuthController.java
@@ -2,11 +2,22 @@ package com.cos.costargram.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.costargram.service.AuthService;
+import com.cos.costargram.utils.Script;
+import com.cos.costargram.web.auth.dto.UserJoinReqDto;
+
+import lombok.RequiredArgsConstructor;
 
 
 // 시작 주소: /auth
+@RequiredArgsConstructor
 @Controller
 public class AuthController {
+	
+	private final AuthService authService;
 
 	@GetMapping("/auth/loginForm")
 	public String loginForm() {
@@ -16,5 +27,11 @@ public class AuthController {
 	@GetMapping("/auth/joinForm")
 	public String joinForm() {
 		return "auth/joinForm";
+	}
+	
+	@PostMapping("/auth/join" )
+	public @ResponseBody String join(UserJoinReqDto userJoinReqDto) {
+		authService.회원가입(userJoinReqDto);
+		return Script.href("성공", "/auth/loginForm");
 	}
 }
